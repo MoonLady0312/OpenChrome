770ec6116475d4256727efcd750d364319f315f4
Misaki Bear <BearOfficeDev@gmail.com>
2022-01-06 18:14:16 +0900

    Update dependency.

--- OpenChrome/Program.cs
+++ OpenChrome/Program.cs
@@ -1,13 +1,9 @@
 ﻿using System.Diagnostics;
-using ConfigReadingLib;
+using BearMLLib;
 
 // Chrome Icon  Artist: firstfear(firstfear.deviantart.com)
 
-try
-{
-    var reader = new ConfigReader("Path.txt");
-    var programpath = reader.GetValue("program path");
-    var instancepath = reader.GetValue("instance path");
-    Process.Start(programpath, $"--user-data-dir=\"{instancepath}\"");
-}
-catch { }
+var reader = new BearML("path.txt");
+var programPath = reader.GetContent<string>("program");
+var instancePath = reader.GetContent<string>("instance");
+Process.Start(programPath, $"--user-data-dir=\"{instancePath}\"");
--- README.md
+++ README.md
@@ -4,7 +4,7 @@
  Usually, create a chrome shortcut manually with args --user-data-dir="{instancepath}" to create or open a chrome instance will work out well.  
  But this method won't work if the shortcut pinned to taskbar or Start.  
  
- Using library [ConfReaderLib](https://github.com/MisakiBear/ConfReaderLib)  
+ Using library [BearMLLib](https://github.com/BearOffice/BearMarkupLanguageLib) to read configs.   
 
 # How to use
- Modify the "Path.txt" file to use.
+ Modify the "path.txt" file to use.
